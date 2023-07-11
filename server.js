b28c58a535f14cc04ffea4c6151aa9181d7ea83c
Agrimray <69646734+Agrimray@users.noreply.github.com>
2023-07-11 11:25:17 +0530

    saap

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const path=require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, AWS Elastic Beanstalk!');
